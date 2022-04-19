939e8737b6e8f912fb0e6aa075d23959b81cba8b
Hugo Protsch <hugo.protsch@gmail.com>
2022-04-19 20:20:00 +0200

    Add pandoc config

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# Verteilte Systeme
+---
+title: "Verteilte Systeme"
+author: [Hugo Protsch, Justin Hoffmann]
+date: "2022-04-19"
+keywords: [Verteilte Systeme, VS]
+...
+
+Here is the actual document text...
 
 
---
+++ pandoc.sh
@@ -0,0 +1,12 @@
+#coverts README.md to PDF with pandoc and eisvogel template
+
+docker run --rm \
+    -v `pwd`:/data \
+    -w /data \
+    rstropek/pandoc-latex \
+    -f markdown \
+    --template https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template/v2.0.0/eisvogel.tex \
+    -t latex \
+    -o README.pdf \
+    README.md
+
